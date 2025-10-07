15bd10ea2918dc94cc7d79764b222b87617f7082
MauS-code <mateussousa.1128@gmail.com>
2025-10-07 17:33:40 -0300

    Add files via upload

---
+++ UC 09 ATV 07/Desafio-1.py
@@ -0,0 +1,9 @@
+def dividir(a, b):
+
+    if b <= 0:
+        return "ERRO!: Não é possivel fazer divisão por zero!"
+    return a / b   
+a = 10
+b = 0 
+resultado = dividir(a, b)
+print(f"O resultado da divisão é: {resultado}")
